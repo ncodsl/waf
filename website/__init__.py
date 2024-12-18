aa4a482e62b8c98a8785a0c9248a18e4e5937eb0
Nc0dsl12 <119877994+Nc0dsl12@users.noreply.github.com>
2024-12-18 17:49:47 -0600

    l

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_pymongo import pyMongo
+from flask_pymongo import PyMongo
 from flask_login import LoginManager
 import os
 from dotenv import load_dotenv
