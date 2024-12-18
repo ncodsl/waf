e5d7726b9ccdcf85ec37e312e57640bbd09889d8
Nc0dsl12 <119877994+Nc0dsl12@users.noreply.github.com>
2024-12-18 12:13:09 -0600

    l

--- website/__init__.py
+++ website/__init__.py
@@ -42,7 +42,19 @@ def create_app():
     
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    from .models import User, Note
     
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
 def create_database(app):
